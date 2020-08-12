80350598c7ed20c017753dadbfcebbae1d2a97fc
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    kunlun2: create dummy libqti-perfd-client
        * CameraHAL complains about missing perfd and then crashes, so let's
        * create a dummy libqti-perfd-client to shut up the hal
        * we could even map the functions to use libperfmgr powerhints in future
    
        08-12 10:21:24.212 E/CHIUSECASE(22011): [ERROR  ] chxutils.cpp:628 LibMap() Failed to load library libqti-perfd-client.so error dlopen failed: library libqti-perfd-client.so not found
        08-12 10:21:24.212 E/CHIUSECASE(22011): [ERROR  ] chxextensionmodule.cpp:181 ExtensionModule() Failed to load perf lib
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I3e1290307c1d20de2a738c02628a74a1e1732e32

--- device.mk
+++ device.mk
@@ -281,6 +281,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
+# Perf dummy
+PRODUCT_PACKAGES += \
+    libqti-perfd-client \
+
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
