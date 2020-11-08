7315c35c608d17494f37a2de37a22665e49f0707
lucky <ah440099@gmail.com>
2020-11-09 00:04:57 +0200

    kunlun2: Add Gcam GO with night sight

--- device.mk
+++ device.mk
@@ -191,6 +191,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    GCam \
     libxml2 \
     libdng_sdk \
     libdng_sdk.vendor \
