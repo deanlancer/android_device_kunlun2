c70c00ec76193b7bc8ea7b5ed183822d8a138813
Chenyang Zhong <zhongcy95@gmail.com>
2020-11-15 03:46:16 -0500

    kunlun2: build 64bit vendor variant of libavservices_minijail
    F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/ims_rtp_daemon": library "libavservices_minijail.so" not found: needed by main executable
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -541,6 +541,8 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+	libavservices_minijail \
+    libavservices_minijail.vendor \
     libc2dcolorconvert \
     libmm-omxcore \
     libOmxAacEnc \
