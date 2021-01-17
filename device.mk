53b9c0547ca4df24ccce4b9dddf5fbb664254cd6
deanlancer <deanlancer@gmail.com>
2021-01-17 15:19:19 +0300

    kunlun2:device: Build libhidltransport and libhwbinder

--- device.mk
+++ device.mk
@@ -504,7 +504,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhwbinder
 
 # IMS
 PRODUCT_PACKAGES += \
