9eeef6379cc95e32a402195d31dd8798d9005687
Ethan Chen <intervigil@gmail.com>
2021-03-01 10:33:27 +0300

    product_props: better ram management

--- product_prop.mk
+++ product_prop.mk
@@ -102,7 +102,20 @@ PRODUCT_PRODUCT_PROPERTIES += \
 	dalvik.vm.dex2oat-resolve-startup-strings=true \
 	dalvik.vm.dex2oat-max-image-block-size=524288 \
 	dalvik.vm.minidebuginfo=true \
-	dalvik.vm.dex2oat-minidebuginfo=true 
+	dalvik.vm.dex2oat-minidebuginfo=true
+
+    # Better Ram
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+ 
 
 #Enable 16 bit PCM offload by default
 PRODUCT_PRODUCT_PROPERTIES += \
