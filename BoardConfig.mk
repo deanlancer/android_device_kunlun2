0c4cfbab26e0008f92e684645c9ed786d2e0b610
Deanlancer <deanlancer@gmail.com>
2021-03-02 10:24:02 +0300

    BoardConfig: delete BUILD_BROKEN_PHONY_TARGETS flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,7 +162,6 @@ DEXPREOPT_GENERATE_APEX_IMAGE := true
 
 # Broken rules#######################################################################
 BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_PHONY_TARGETS := true
 BUILD_BROKEN_USES_NETWORK := true
 BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES := true
