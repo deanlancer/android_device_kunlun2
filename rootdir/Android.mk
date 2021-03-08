bcae3211d7f77d19e616d29454571643993eeea4
Deanlancer <deanlancer@gmail.com>
2021-03-08 20:24:24 +0300

    kunlun2: delete loggy

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -251,14 +251,6 @@ include $(BUILD_PREBUILT)
 
 # testing for logs
 include $(CLEAR_VARS)
-LOCAL_MODULE       := loggy.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/loggy.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := logcat.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
--- rootdir/bin/loggy.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-# loggy.sh.
-
-logcat -b all -v time -f  /cache/loggy_logcat.txt & dmesg -w > /cache/loggy_kmsg.txt
