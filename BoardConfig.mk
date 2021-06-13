28b68c312b0f16210b9278f342487b6f91c2cfbf
Deanlancer <deanlancer@gmail.com>
2021-06-13 15:47:24 +0300

    kunlun2: initial commit for sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,6 +219,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 SELINUX_IGNORE_NEVERALLOWS := true
 
