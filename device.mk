84b22b9f7cf615a2b548ba4550894dd1ad86a8f2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-19 11:46:02 +0200

    vibrator: Rebrand to Lenovo
    Change-Id: Icd0ec7c857574caec085751253fafa39be065d0d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -486,6 +486,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.lenovo
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
