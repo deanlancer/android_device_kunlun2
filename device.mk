b5a3b41f601bdc7aa7ecbf4117c0d60062423457
Deanlancer <deanlancer@gmail.com>
2021-09-14 09:07:06 +0300

    overlay: remove NoCutoutOverlay

--- device.mk
+++ device.mk
@@ -138,7 +138,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aicp
-PRODUCT_PACKAGES += NoCutoutOverlay
 	
 
 # Audio
