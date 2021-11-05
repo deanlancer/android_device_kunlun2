189c746ff40ccf750d2b1f25f9f0fe7b2634c1e9
Deanlancer <deanlancer@gmail.com>
2021-11-05 10:51:04 +0300

    [TEMP] properties: set debugging on

--- properties/product_prop.mk
+++ properties/product_prop.mk
@@ -91,7 +91,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.audio.fluence.voicerec=true
 
 # Eng
-ifeq ($(TARGET_BUILD_VARIANT),eng)
+ifeq ($(TARGET_BUILD_VARIANT),userdebug)
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.usb.config=mtp,adb \
     ro.secure=0 \
@@ -106,8 +106,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.persist.partition.support=no
 endif
 # Userdebug
-ifeq ($(TARGET_BUILD_VARIANT),userdebug)
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.secure=1 \
-    ro.adb.secure=1
-endif
+# ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+# PRODUCT_PRODUCT_PROPERTIES += \
+#     ro.secure=1 \
+#     ro.adb.secure=1
+# endif
