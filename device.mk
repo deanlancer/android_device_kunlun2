9b294ee7df81806653881fa2e3058b7cb9d45c44
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    kunlun2: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261

--- device.mk
+++ device.mk
@@ -62,7 +62,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1-impl:32 \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
