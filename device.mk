01a2554d4f7bf5951aa32b353f3d5732b38ed24d
Deanlancer <deanlancer@gmail.com>
2022-02-23 12:05:54 +0300

    kunlun2: Delete ipcm

--- device.mk
+++ device.mk
@@ -619,5 +619,4 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml \
-    $(LOCAL_PATH)/configs/IPACM_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/IPACM_cfg.xml \
     $(LOCAL_PATH)/configs/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf
