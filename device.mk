35ad92f54680ef256c33b245cf66fa99e63b2cd1
Deanlancer <deanlancer@gmail.com>
2022-06-01 18:27:34 +0300

    Revert "device: Include Lawnchair when available"
    This reverts commit 8916819ff5fc2b15b2da79e69da75baf12f748eb.

--- device.mk
+++ device.mk
@@ -21,9 +21,6 @@ ifeq ($(ADD_MICROG), true)
 $(call inherit-product-if-exists, vendor/lenovo/MicroG/config.mk)
 endif
 
-# ArrowOS Properties
-$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
-
 # Hardware
 PRODUCT_BOARD_PLATFORM := sdm845
 PRODUCT_USES_QCOM_HARDWARE := true
