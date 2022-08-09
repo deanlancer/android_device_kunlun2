cef312611a22db5821797d190586dccb8ec46a27
Deanlancer <deanlancer@gmail.com>
2022-08-09 11:56:41 +0300

    device: remove QTI vibrator

--- device.mk
+++ device.mk
@@ -535,10 +535,6 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.5.vendor \
