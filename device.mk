563a06db38520747cac27a6a8582c7873055afde
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    device: Migrate to AIDL ClearKey DRM HAL

--- device.mk
+++ device.mk
@@ -288,7 +288,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # GNSS
 PRODUCT_PACKAGES += \
