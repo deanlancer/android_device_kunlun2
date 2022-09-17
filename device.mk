906d226413f2929c3520c79750b3ec8757522c52
Deanlancer <deanlancer@gmail.com>
2022-09-17 23:56:32 +0300

    kunlun2: use prebuilt libtinyxml
    This reverts commit 3dd6c7d74a15fa51dae861f820e6493eac14ff42

--- device.mk
+++ device.mk
@@ -266,7 +266,6 @@ PRODUCT_PACKAGES += \
     libhwc2onfbadapter \
     libqdMetaData \
     libqdMetaData.vendor \
-    libtinyxml \
     libvulkan \
     memtrack.sdm710 \
     vendor.display.config@1.0.vendor \
