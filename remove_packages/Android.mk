2cd82a79a0a5d08806464d6f40e4fd52ae10a3c6
Deanlancer <deanlancer@gmail.com>
2022-10-24 17:05:31 +0300

    kunlun2: remove Updater app

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := remove_packages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := MusicFX Browser2 ExactCalculator Calculator
+LOCAL_OVERRIDES_PACKAGES := MusicFX Browser2 ExactCalculator Calculator Updater
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
