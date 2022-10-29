7b386a3cf9759fac9b1e0bd3c1ceec2ddff89987
Deanlancer <deanlancer@gmail.com>
2022-10-29 09:25:05 +0300

    [riceDroid] kunlun2: remove prebuilt DevicePersonalization app

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := remove_packages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt FlipendoPrebuilt TurboPrebuilt DevicePersonalizationPrebuiltPixel2021 DeviceIntelligenceNetworkPrebuilt Eleven AudioFX Updater OPWidget
+LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt FlipendoPrebuilt TurboPrebuilt DevicePersonalizationPrebuiltPixel2022AOSP DeviceIntelligenceNetworkPrebuilt Eleven AudioFX Updater OPWidget
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
